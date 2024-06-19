9f06ffcdfb41bf7befae466f4a9be95fa5fae248
Maryam <khani.mkh@gmail.com>
2024-06-18 20:18:12 -0400

    Display analysis results

--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -48,7 +48,14 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
+        result = cv_client.analyze(
+        image_data=image_data,
+        visual_features=[
+        VisualFeatures.CAPTION,
+        VisualFeatures.DENSE_CAPTIONS,
+        VisualFeatures.TAGS,
+        VisualFeatures.OBJECTS,
+        VisualFeatures.PEOPLE],)
 
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
@@ -56,6 +63,24 @@ def AnalyzeImage(image_filename, image_data, cv_client):
         print(f"Message: {e.error.message}")
 
     # Display analysis results
+    # Get image captions
+    if result.caption is not None:
+     print("\nCaption:")
+     print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+
+   # Get image dense captions
+    if result.dense_captions is not None:
+     print("\nDense Captions:")
+     for caption in result.dense_captions.list:
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+
+   # Get image tags
+
+
+   # Get objects in the image
+
+
+   # Get people in the image
     
 
 def BackgroundForeground(endpoint, key, image_file):
